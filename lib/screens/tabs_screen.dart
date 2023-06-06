d89d473910fb53bd949559f97431b58f25dfd259
Lucas Muniz Lara <lucasml@live.com>
2023-06-06 11:16:33 -0300

    icons on drawer with function

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
+import '../components/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
   @override
   State<TabsScreen> createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedScreenIndex = 0;
-  final List<Map<String, Object>> _screens = [
+  final List<Map<String, Object>> _screens = const [
     {'title': 'Lista de categorias', 'screen': CategoriesScreen()},
     {'title': 'Meus Favoritos', 'screen': FavoriteScreen()}
   ];
@@ -28,7 +31,7 @@ class _TabsScreenState extends State<TabsScreen> {
         unselectedItemColor: Theme.of(context).colorScheme.secondary,
         selectedItemColor: Colors.pink,
         currentIndex: _selectedScreenIndex,
-        items: [
+        items: const [
           BottomNavigationBarItem(
               icon: Icon(Icons.category), label: 'Categorias'),
           BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritos'),
@@ -38,6 +41,7 @@ class _TabsScreenState extends State<TabsScreen> {
         title: Text(_screens[_selectedScreenIndex]['title'] as String),
       ),
       body: _screens[_selectedScreenIndex]['screen'] as Widget,
+      drawer: MainDrawer(),
     );
   }
 }
