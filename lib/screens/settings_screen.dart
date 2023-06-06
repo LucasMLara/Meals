d880ed1a1cc4e7b7b8d5afe176c61ecb8c6f7133
Lucas Muniz Lara <lucasml@live.com>
2023-06-06 12:08:35 -0300

    replace navigation method

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/main_drawer.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -6,6 +7,7 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const MainDrawer(),
       appBar: AppBar(
           title: const Text(
         'Configurações',
